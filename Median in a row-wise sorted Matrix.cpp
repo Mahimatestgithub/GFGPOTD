423d39790d1b2b4733f31687a715e0fd87014a1b
Mahima Gupta <131277652+Mahimatestgithub@users.noreply.github.com>
2025-08-22 23:47:04 +0530

    Create Median in a row-wise sorted Matrix.cpp

---
+++ Median in a row-wise sorted Matrix.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int median(vector<vector<int>> &mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        int low = INT_MAX, high = INT_MIN;
+        
+        // Find min and max in matrix
+        for(int i = 0; i < n; i++) {
+            low = min(low, mat[i][0]);        // first element of row
+            high = max(high, mat[i][m-1]);    // last element of row
+        }
+        
+        int desired = (n * m + 1) / 2;
+        
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            
+            // Count how many numbers <= mid
+            int cnt = 0;
+            for(int i = 0; i < n; i++) {
+                cnt += upper_bound(mat[i].begin(), mat[i].end(), mid) - mat[i].begin();
+            }
+            
+            if(cnt < desired) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+};
